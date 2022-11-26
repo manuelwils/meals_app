041c7de0975213ac939f72e2f22533d4597129fa
Manuelwils <manuelgwilz@gmail.com>
2022-11-26 04:26:36 -0300

    setting up app theme

---
+++ lib/categories_screens.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/category_item.dart';
+
+import './dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 1.5,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        padding: const EdgeInsets.all(10),
+        children: dummyCategories
+            .map((catData) => CategoryItem(catData.title!, catData.color))
+            .toList(),
+      ),
+    );
+  }
+}
